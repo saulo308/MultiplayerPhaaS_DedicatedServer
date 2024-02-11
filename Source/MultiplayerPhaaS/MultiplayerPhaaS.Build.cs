6b8986fd6b773c0e98d249c344019d833d63858c
Saulo Soares <35870646+saulo308@users.noreply.github.com>
2024-02-11 16:20:03 -0300

    Including jolt physics plugin

--- Source/MultiplayerPhaaS/MultiplayerPhaaS.Build.cs
+++ Source/MultiplayerPhaaS/MultiplayerPhaaS.Build.cs
@@ -7,8 +7,10 @@ public class MultiplayerPhaaS : ModuleRules
 	public MultiplayerPhaaS(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] 
+        bEnableUndefinedIdentifierWarnings = false;
+        CppStandard = CppStandardVersion.Cpp17;
+
+        PublicDependencyModuleNames.AddRange(new string[] 
 		{ 
 			"Core", 
 			"CoreUObject", 
@@ -17,7 +19,8 @@ public class MultiplayerPhaaS : ModuleRules
 			"UMG",
 			"OnlineSubsystem",
             "OnlineSubsystemSteam",
-			"RemotePhysicsEngineSystem"
+			"RemotePhysicsEngineSystem",
+            "JoltPhysicsWrapper"
         });
 
 		PrivateDependencyModuleNames.AddRange(new string[] 
